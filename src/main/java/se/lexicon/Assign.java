70cad19d0cf4a9afe065dd51ada60718e19922d2
Dena Aghajari <d.aghajari1234@gmail.com>
2021-05-28 11:44:12 +0200

    assignment calculator (done)

--- src/main/java/se/lexicon/Assign.java
+++ src/main/java/se/lexicon/Assign.java
@@ -7,68 +7,78 @@ public class Assign {
     // methods - behaviors
 
     public static void main(String[] args) {
-        System.out.println("Wellcome to the calculator");
-        System.out.println("for addition press 'ad', for subtraction press 's'");
-        System.out.println("for division press 'd',for multiplication press 'm'");
-        System.out.println("press no to end");
+
+        boolean check=true;
+        while(check) {
+            print();
+
+            Scanner scan = new Scanner(System.in);
+            String press = scan.nextLine();
+            if (press.equals("no")){
+                break;
+            }
+            System.out.println("Enter two numbers:");
+
+
+
+            switch (press) {
+                case "ad":
+                    addition();
+                    break;
+
+                case "s":
+                    subtraction();
+                    break;
+                case "d":
+                    division();
+                    break;
+                case "m":
+                   multiplication();
+                    break;
+
+                default:
+                    System.out.println("operation invalid");
+
+            }
+        }
+        System.out.println("done");
+        }
+
+        public static void print() {
+            System.out.println("wellcome");
+            System.out.println("for addition press 'ad', for subtraction press 's'");
+            System.out.println("for division press 'd',for multiplication press 'm'");
+            System.out.println("press no to end");
+        }
+       public static void addition(){
+           Scanner scan = new Scanner(System.in);
+           int a = scan.nextInt();
+           int b = scan.nextInt();
+           int sum = a + b;
+           System.out.println(a + "+" + b + "=" + sum);
+       }
+    public static void subtraction(){
         Scanner scan = new Scanner(System.in);
-        String press=scan.nextLine();
-        if ("ad".equals(press)) {
-            addition();
-        }
-        if ("s".equals(press)) {
-            subtraction();
-        }
-        if ("d".equals(press)) {
-            division();
-        }
-        if ("m".equals(press)) {
-            multiplication();
-        }
-        if("No".equals(press)){
-            System.out.println("Bye");
-
-        }
-        }
-
-
-    public static void addition() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter two numbers:");
         int a = scan.nextInt();
         int b = scan.nextInt();
-        int sum = a + b;
-        System.out.println(a + "+" + b + "=" + sum);
 
-    }
-
-    public static void subtraction() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter two numbers:");
-        int a = scan.nextInt();
-        int b = scan.nextInt();
         int sub = a - b;
         System.out.println(a + "-" + b + "=" + sub);
     }
-
-    public static void multiplication() {
+    public static void division(){
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter two numbers:");
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int mul = a * b;
-        System.out.println(a + "*" + b + "=" + mul);
-    }
-
-    public static void division() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter two numbers:");
         int a = scan.nextInt();
         int b = scan.nextInt();
         double div = a / b;
         System.out.println(a + "/" + b + "=" + div);
-
-
     }
-}
+    public static void multiplication(){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();        int mul = a * b;
+        System.out.println(a + "*" + b + "=" + mul);
+    }
+    }
+
+
 
